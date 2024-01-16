29cf877dfd91e4ff2062b4ee8b6aad1fcb93604b
Xavier Ruiz <xruiz@bu.edu>
2024-01-15 20:42:44 -0500

    step(introduction): set up environment and implement initial provider type

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -11,7 +11,10 @@ import (
 )
 
 // testAccProtoV6ProviderFactories are used to instantiate a provider during
-// acceptance testing. The factory function will be invoked for every Terraform
+// acceptance testing.
+// TODO: what is acceptance testing?
+//
+// The factory function will be invoked for every Terraform
 // CLI command executed to create a provider server to which the CLI can
 // reattach.
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
