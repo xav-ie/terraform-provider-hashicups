309bb6264613f7bfbb4083a4f9bc93b39605da9d
xav-ie <xruizify@gmail.com>
2024-01-16 08:43:57 -0500

    step(logging): complete

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
 // Ensure the implementation satisfies the expected interfaces.
@@ -66,6 +67,7 @@ type hashicupsProviderModel struct {
 
 // Configure prepares a HashiCups API client for data sources and resources.
 func (p *hashicupsProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	tflog.Info(ctx, "Configuring HashiCups client")
 	// Retrieve provider data from configuration
 	var config hashicupsProviderModel
 	diags := req.Config.Get(ctx, &config)
@@ -163,6 +165,12 @@ func (p *hashicupsProvider) Configure(ctx context.Context, req provider.Configur
 	if resp.Diagnostics.HasError() {
 		return
 	}
+	ctx = tflog.SetField(ctx, "hashicups_host", host)
+	ctx = tflog.SetField(ctx, "hashicups_username", username)
+	ctx = tflog.SetField(ctx, "hashicups_password", password)
+	ctx = tflog.MaskFieldValuesWithFieldKeys(ctx, "hashicups_password")
+
+	tflog.Debug(ctx, "Creating HashiCups client")
 
 	// Create a new HashiCups client using the configuration values
 	client, err := hashicups.NewClient(&host, &username, &password)
@@ -180,6 +188,8 @@ func (p *hashicupsProvider) Configure(ctx context.Context, req provider.Configur
 	// type Configure methods.
 	resp.DataSourceData = client
 	resp.ResourceData = client
+	tflog.Info(ctx, "Configured HashiCups client", map[string]any{"success": true})
+
 }
 
 // DataSources defines the data sources implemented in the provider.
